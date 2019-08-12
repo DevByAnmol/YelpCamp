f5bce00eabf389d2ff381dcf49821a4d639a7693
Anmol2611 <anmolyadav084@gmail.com>
2019-08-12 21:14:46 +0530

    Added process.env.PORT and process.env.IP to listen route

--- app.js
+++ app.js
@@ -60,6 +60,13 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/", indexRoutes);
 
-app.listen("3000", function(){
+// For Deployment
+app.listen(process.env.PORT, process.env.IP,function(){
     console.log("The YelpCamp Server has started!!");
 });
+
+// For Local Development
+
+// app.listen("3000", function(){
+//     console.log("The YelpCamp Server has started!!");
+// });
